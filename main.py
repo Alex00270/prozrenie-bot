31b499a2201d43c0f56f125861fc34e956b1e021
Alex00270 <34465879+Alex00270@users.noreply.github.com>
2025-12-12 11:17:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-
-Search
-Search
-
-Last hour
-GMT+2
-
-Menu
-
-     ==> Deploying...
-    from aiogram.filters import F
-ImportError: cannot import name 'F' from 'aiogram.filters' (/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/filters/__init__.py)
-     ==> Exited with status 1
+TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
      ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
 ==> Running 'python main.py'
 Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 3, in <module>
-    from aiogram.filters import F
-ImportError: cannot import name 'F' from 'aiogram.filters' (/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/filters/__init__.py)
+  File "/opt/render/project/src/main.py", line 16, in <module>
+    bot = Bot(token=TOKEN, parse_mode="HTML")
+  File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
+    raise TypeError(
+    ...<3 lines>...
+    )
+TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
      ==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
      ==> Deploying...
 ==> Running 'python main.py'
@@ -44,8 +36,9 @@ TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content`
      ==> No open ports detected, continuing to scan...
      ==> Docs on specifying a port: https://render.com/docs/web-services#port-binding
 ==> Running 'python main.py'
+INFO:__main__:Starting bot initialization...
 Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 16, in <module>
+  File "/opt/render/project/src/main.py", line 18, in <module>
     bot = Bot(token=TOKEN, parse_mode="HTML")
   File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
     raise TypeError(
@@ -55,55 +48,3 @@ TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content`
      ==> Exited with status 1
      ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
 ==> Running 'python main.py'
-Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 16, in <module>
-    bot = Bot(token=TOKEN, parse_mode="HTML")
-  File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
-    raise TypeError(
-    ...<3 lines>...
-    )
-TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
-     ==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
-     ==> Deploying...
-==> Running 'python main.py'
-Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 16, in <module>
-    bot = Bot(token=TOKEN, parse_mode="HTML")
-  File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
-    raise TypeError(
-    ...<3 lines>...
-    )
-TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
-     ==> Exited with status 1
-     ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
-==> Running 'python main.py'
-Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 16, in <module>
-    bot = Bot(token=TOKEN, parse_mode="HTML")
-  File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
-    raise TypeError(
-    ...<3 lines>...
-    )
-TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
-     ==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
-     ==> Deploying...
-==> Running 'python main.py'
-Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 16, in <module>
-    bot = Bot(token=TOKEN, parse_mode="HTML")
-  File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
-    raise TypeError(
-    ...<3 lines>...
-    )
-TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
-     ==> Exited with status 1
-     ==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
-==> Running 'python main.py'
-Traceback (most recent call last):
-  File "/opt/render/project/src/main.py", line 16, in <module>
-    bot = Bot(token=TOKEN, parse_mode="HTML")
-  File "/opt/render/project/src/.venv/lib/python3.13/site-packages/aiogram/client/bot.py", line 304, in __init__
-    raise TypeError(
-    ...<3 lines>...
-    )
-TypeError: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is not supported anymore. These arguments have been removed in 3.7.0 version. Use `default=DefaultBotProperties(parse_mode='HTML')` argument instead.
